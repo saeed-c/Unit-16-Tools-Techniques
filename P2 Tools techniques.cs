b3c5b28d307a0b5675e5cc968cda8d1c3420b2f9
saeed-c <55282225+saeed-c@users.noreply.github.com>
2020-01-07 10:58:59 +0000

    Add files via upload

---
+++ P2 Tools techniques.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type first number...");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type *,/,+,-");
+            string z = Console.ReadLine();
+            Console.WriteLine("Type second number...");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (z == "+")
+            {
+                Console.WriteLine("Answer: {0}", x + y);
+            }
+            if (z == "-")
+            {
+                Console.WriteLine("Answer: {0}", x - y);
+            }
+            if (z == "*")
+            {
+                Console.WriteLine("Answer: {0}", x * y);
+            }
+            if (z == "/")
+            {
+                Console.WriteLine("Answer: {0}", x / y);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
